3c1d94bb5c151a5bb73f6e432a5c7ce9e1635840
rmit-s3604367-johnny-huynh <s3604367@student.rmit.edu.au>
2017-03-14 00:36:16 +1100

    Debug: git pull only
    Former-commit-id: b61060ef1a4d5368f85b95373d6bed763fb4b06f

--- public/deploy.php
+++ public/deploy.php
@@ -1,6 +1,6 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 // if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
-echo shell_exec('cd C:\web\www\SE-PT-Assignment && dir');
+echo shell_exec('cd C:\web\www\SE-PT-Assignment && git pull');
 // }
  ?>
