a25a981fedab50d06402fda7043a22294a44417a
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2017-03-19 22:03:45 +1100

    Changed placeholder
    Former-commit-id: 39ce185a6697de98e384996e90ff58aa40e641c0

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -11,15 +11,15 @@
 				<label for="inputFirstName">First Name</label>
 				<input type="text" id="inputFirstName" class="form-control login__input" placeholder="First Name" required autofocus>
 				<label for="inputLastName">Last Name</label>
-				<input type="text" id="inputLastName" class="form-control login__input" placeholder="First Name" required autofocus>
+				<input type="text" id="inputLastName" class="form-control login__input" placeholder="Last Name" required autofocus>
 				<label for="inputUsername">Username</label>
 				<input type="text" id="inputUsername" class="form-control login__input" placeholder="Username" required autofocus>
 				<label for="inputPassword">Password</label>
 				<input type="password" id="inputPassword" class="form-control login__input" placeholder="Password" required>
 				<label for="inputPhone">Phone</label>
-				<input type="text" id="inputPhone" class="form-control login__input" placeholder="Username" required autofocus>
+				<input type="text" id="inputPhone" class="form-control login__input" placeholder="Phone" required autofocus>
 				<label for="inputEmail">Email Address</label>
-				<input type="email" id="inputEmail" class="form-control login__input" placeholder="Username" required autofocus>
+				<input type="email" id="inputEmail" class="form-control login__input" placeholder="Email Address" required autofocus>
 			</form>
 		</div>
 		<a class="btn btn-lg btn-primary btn-block" href="/register">Register</a>
