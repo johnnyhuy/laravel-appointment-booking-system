59b223219e8a3ba83d8af2bec44613ed70b9cfcf
rmit-s3604367-johnny-huynh <s3604367@student.rmit.edu.au>
2017-03-19 14:46:00 +1100

    Structured the login to have username and password
    Former-commit-id: a13e6c007ea5f11b1cfaf433ae061d1190f4e666

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
 	<div class="container">
-		<form class="form-signin">
-			<h2 class="form-signin-heading">Please sign in</h2>
-			<label for="inputEmail" class="sr-only">Email address</label>
-			<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-			<label for="inputPassword" class="sr-only">Password</label>
-			<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-			<div class="checkbox">
-				<label>
-					<input type="checkbox" value="remember-me"> Remember me
-				</label>
-			</div>
-			<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-		</form>
+		<div class="header">
+			<h1 class="header__title">Business Name</h1>
+			<h3 class="header__subtitle">Booking System</h3>
+		</div>
+		<div class="block login">
+			<form class="login__form" method="POST" action="/login">
+				<label for="inputUsername">Username</label>
+				<input type="text" id="inputUsername" class="form-control login__input" placeholder="Username" required autofocus>
+				<label for="inputPassword">Password</label>
+				<input type="password" id="inputPassword" class="form-control login__input" placeholder="Password" required>
+				<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+			</form>
+		</div>
 	</div>
 @endsection
