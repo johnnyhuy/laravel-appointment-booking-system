7dca5693a2012dc5e49775f51ae3841628ca7738
rmit-s3601725-craig-barry <s3601725@student.rmit.edu.au>
2017-03-20 11:07:06 +1100

    Add files via upload
    Updated Controllers
    
    Former-commit-id: d0a658cc4b84d3915c3d59a473db30b994c02c05

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    public static function loginCustomer($username, $password) {
+		return false;
+	}
+	
+	public static function registerCustomer($name, $address, $username, $password, $phone) {
+		return false;
+	}
 }
