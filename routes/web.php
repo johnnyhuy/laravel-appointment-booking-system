0a79ce469c237d321debc861ee3dffb24d99ffc5
rmit-s3601725-craig-barry <s3601725@student.rmit.edu.au>
2017-03-20 23:59:06 +1100

    Add files via upload
    Former-commit-id: 26ab59e93acfd15989e16eb7c817be75058207b0

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,11 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
- */
+*/
 
 Route::get('/', 'Auth\LoginController@index');
 Route::get('/login', 'Auth\LoginController@index');
 Route::get('/register', 'Auth\RegisterController@index');
-
 Route::post('/login', 'Auth\LoginController@create');
+Route::post('/register', 'CustomerController@create');
+
