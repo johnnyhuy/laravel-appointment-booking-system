49e10ceb20fda23407fc86a1e2d98d17b0749186
Lachlan-Porter <s3537901@student.rmit.edu.au>
2017-03-21 00:56:00 +1100

    refactored code and added test functionality
    Former-commit-id: 3700467cc46810ffcd66550a192e43bd84dcd096

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -12,22 +12,38 @@
 					<th>Title</th>
 					<th>Start Time</th>
 					<th>End Time</th>
+					<th>Customer id</th>
 				</tr>
 				<?php
-					#temp for testing
-					$customerID = 1;
-					$bookings = DB::table('bookings')->get();
-            		#echo "$bookings";
+					/*
+						Testing stuff
+							-first we fill the DB with sample data
+							-then we pick a random customerID (from 1 to 10)
+							-then display bookings with this customerid
+							-This shows the functionality of pulling bookings from the DB
+					*/
+					#fills the database with test data
+					function fillDBTest()
+					{
+						for($i = 0; $i<100; $i++)
+						{
+							factory(App\Booking::class)->create();
+						}
+					}
+					#uncomment this to have the database be filled
+					#fillDBtest();
+					$customerID = rand(1,10);
+					$bookings = DB::table('bookings')
+					->where('customer_id', '=', $customerID)
+					->get();
 				?>
 				@foreach ($bookings as $booking)
-					@if($booking->customer_id == $customerID)
-						<tr>
-							<td>{{$booking->title}}</td>
-							<td>{{$booking->booking_start_time}}</td>
-							<td>{{$booking->booking_end_time}}</td>	
-							<td>{{$booking->customer_id}}</td>
-						</tr>
-					@endif
+					<tr>
+						<td>{{$booking->title}}</td>
+						<td>{{$booking->booking_start_time}}</td>
+						<td>{{$booking->booking_end_time}}</td>	
+						<td>{{$booking->customer_id}}</td>
+					</tr>
 				@endforeach
 			</table>
 		</div>
