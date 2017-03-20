430a5154270e5196197f4d1cdc12a99849ca5fba
rmit-s3601725-craig-barry <s3601725@student.rmit.edu.au>
2017-03-21 00:00:06 +1100

    Add files via upload
    Former-commit-id: 8876dfb73a5055418392a0e65bf3e032e5516769

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -7,21 +7,22 @@
 			<h3 class="header__subtitle">Booking System</h3>
 		</div>
 		<div class="block login">
-			<form class="login__form" method="POST" action="/login">
+			<form class="login__form" method="post" action="/register">
 				<label for="inputFirstName">First Name</label>
-				<input type="text" id="inputFirstName" class="form-control login__input" placeholder="First Name" required autofocus>
+				<input type="text" id="inputFirstName" class="form-control login__input" placeholder="First Name" name="firstName" required autofocus>
 				<label for="inputLastName">Last Name</label>
-				<input type="text" id="inputLastName" class="form-control login__input" placeholder="Last Name" required autofocus>
+				<input type="text" id="inputLastName" class="form-control login__input" placeholder="Last Name" name="lastName" required autofocus>
 				<label for="inputUsername">Username</label>
-				<input type="text" id="inputUsername" class="form-control login__input" placeholder="Username" required autofocus>
+				<input type="text" id="inputUsername" class="form-control login__input" placeholder="Username" name="username" required autofocus>
 				<label for="inputPassword">Password</label>
-				<input type="password" id="inputPassword" class="form-control login__input" placeholder="Password" required>
+				<input type="password" id="inputPassword" class="form-control login__input" placeholder="Password" name="password" required>
 				<label for="inputPhone">Phone</label>
-				<input type="text" id="inputPhone" class="form-control login__input" placeholder="Phone" required autofocus>
-				<label for="inputEmail">Email Address</label>
-				<input type="email" id="inputEmail" class="form-control login__input" placeholder="Email Address" required autofocus>
+				<input type="text" id="inputPhone" class="form-control login__input" placeholder="Phone" name="phone" required autofocus>
+				<label for="inputAddress">Address</label>
+				<input type="text" id="inputAddress" class="form-control login__input" placeholder="Address" name="address" required autofocus>
+				<input type="hidden" name="_token" value="{{ csrf_token() }}">
+				<input type="submit" class="btn btn-lg btn-primary btn-block" value="Register"></input>
 			</form>
 		</div>
-		<a class="btn btn-lg btn-primary btn-block" href="/register">Register</a>
 	</div>
 @endsection
