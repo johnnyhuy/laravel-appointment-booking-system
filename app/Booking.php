d655e08d4ae3421a7b5f58c641f736e50b51278b
rmit-s3601725-craig-barry <s3601725@student.rmit.edu.au>
2017-03-20 11:05:28 +1100

    Add files via upload
    Updated Models
    
    Former-commit-id: d38cfdfea99f35f3bd7a4a3100259001dc23cfde

--- app/Booking.php
+++ app/Booking.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+	public function scopeGetAvailableBookings($query) {
+		return false;
+	}
+
+	public function scopeGetBookingsSummary($query) {
+		return [false];
+	}
 }
