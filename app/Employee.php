13c03b339692e17c260bf4fc320b750656b3d79f
rmit-s3601725-craig-barry <s3601725@student.rmit.edu.au>
2017-03-20 11:09:47 +1100

    Add files via upload
    Updated Models
    
    Former-commit-id: db3fdfe617394103e289691681c3a6cb6b65abbd

--- app/Employee.php
+++ app/Employee.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Availability;
 
 class Employee extends Model
 {
-    //
+    public function scopeGetEmployee($query, $id) {
+		return false;
+	}
+	
+	public function scopeGetEmployeeAvailability($query, $id) {
+		return [false];
+	}
 }
