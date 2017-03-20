3eade6a6e788a15a11204544784a7f56950018a2
Lachlan-Porter <s3537901@student.rmit.edu.au>
2017-03-20 23:18:54 +1100

    added basic view booking using dummy data
    Former-commit-id: e637bdac89540ac6b25e0d487f33be8593bc128e

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -8,27 +8,27 @@
 		</div>
 		<div class="block">
 			<table class="table">
-				<?php
-					#use factory to create dummy data since login isnt completed at time of writing
-					$customer = factory(App\Customer::class)->create();
-					$booking1 = factory(App\Booking::class)->create();
-					$booking2 = factory(App\Booking::class)->create();
-					$booking3 = factory(App\Booking::class)->create();
-
-					#$bookings = array(booking1, booking2, booking3);
-				?>
 				<tr>
 					<th>Title</th>
-					<th>Date</th>
 					<th>Start Time</th>
 					<th>End Time</th>
 				</tr>
-				<tr>
-					<td>Example</td>
-					<td>10/14/2016</td>
-					<td>10:00AM</td>
-					<td>11:00PM</td>
-				</tr>
+				<?php
+					#temp for testing
+					$customerID = 1;
+					$bookings = DB::table('bookings')->get();
+            		#echo "$bookings";
+				?>
+				@foreach ($bookings as $booking)
+					@if($booking->customer_id == $customerID)
+						<tr>
+							<td>{{$booking->title}}</td>
+							<td>{{$booking->booking_start_time}}</td>
+							<td>{{$booking->booking_end_time}}</td>	
+							<td>{{$booking->customer_id}}</td>
+						</tr>
+					@endif
+				@endforeach
 			</table>
 		</div>
 	</div>
