e6e68b77391fee8f2959307fc4a5bcaf89bd7bee
rmit-s3604367-johnny-huynh <s3604367@student.rmit.edu.au>
2017-03-27 15:07:39 +1100

    Updated CSS and structure for admin dashboard & dashboard test cases
    Former-commit-id: 9eac415561063bb5690ce214485444d8f8cea9d4

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -3,23 +3,16 @@
 
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="description" content="">
 	<meta name="author" content="">
 	<link rel="icon" href="../../favicon.ico">
-	@if ($business->business_name)
-		<title>{{ $business->business_name }}: Dashboard</title>
-	@else
-		<title>Default Dashboard</title>
-	@endif
-	<!-- Reference main css file -->
+	<title>{{ $business->business_name }}: Dashboard</title>
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
-<body>
-	<nav class="navbar navbar-inverse navbar-fixed-top">
+<body class="dashboard">
+	<nav class="navbar navbar-fixed-top">
 		<div class="container-fluid">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -28,18 +21,13 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Booking System</a>
+				<a class="navbar-brand" href="#">{{ $business->business_name }}: Dashboard</a>
 			</div>
 			<div id="navbar" class="navbar-collapse collapse">
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#">Dashboard</a></li>
-					<li><a href="#">Settings</a></li>
-					<li><a href="#">Profile</a></li>
-					<li><a href="#">Help</a></li>
+					<li><a href="#">Logged in as {{ $business->username }}</a></li>
+					<li><a href="/logout">Logout</a></li>
 				</ul>
-				<form class="navbar-form navbar-right">
-					<input type="text" class="form-control" placeholder="Search...">
-				</form>
 			</div>
 		</div>
 	</nav>
@@ -47,8 +35,7 @@
 		<div class="row">
 			<div class="col-sm-3 col-md-2 sidebar">
 				<ul class="nav nav-sidebar">
-					<li class="active"><a href="#">Booking summaries <span class="sr-only">(current)</span></a></li>
-					<li><a href="#">Availability</a></li>
+					<li class="active"><a href="#">Bookings<span class="sr-only">(current)</span></a></li>
 					<li><a href="#">Employees</a></li>
 					<li><a href="#">Roster</a></li>
 				</ul>
--- tests/Browser/BusinessOwnerDashboardTest.php
+++ tests/Browser/BusinessOwnerDashboardTest.php
@@ -20,10 +20,64 @@ class BusinessOwnerDashboardTest extends DuskTestCase
         
         $this->browse(function ($browser) use ($owner) {
         	$firstOwner = BusinessOwner::first();
-        	// dd($firstOwner);
             $browser->loginAs($firstOwner, 'web_admin')
             	->visit('/admin')
+            	->assertPathIs('/admin')
                 ->assertSee($firstOwner->business_name);
+        });
+    }
+
+    /**
+     * Test to visit roster page in dashboard
+     *
+     * @return void
+     */
+    public function testVisitRoster() {
+    	// Generate customer info
+        $owner = factory(BusinessOwner::class)->create();
+        
+        $this->browse(function ($browser) use ($owner) {
+        	$firstOwner = BusinessOwner::first();
+            $browser->loginAs($firstOwner, 'web_admin')
+            	->visit('/admin/roster')
+            	->assertPathIs('/admin/roster')
+                ->assertSee('Roster');
+        });
+    }
+
+    /**
+     * Test to visit employees page in dashboard
+     *
+     * @return void
+     */
+    public function testVisitEmployees() {
+    	// Generate customer info
+        $owner = factory(BusinessOwner::class)->create();
+        
+        $this->browse(function ($browser) use ($owner) {
+        	$firstOwner = BusinessOwner::first();
+            $browser->loginAs($firstOwner, 'web_admin')
+            	->visit('/admin/employees')
+            	->assertPathIs('/admin/employees')
+                ->assertSee('Employees');
+        });
+    }
+
+    /**
+     * Test to visit summary of bookings page in dashboard
+     *
+     * @return void
+     */
+    public function testVisitBookings() {
+    	// Generate customer info
+        $owner = factory(BusinessOwner::class)->create();
+        
+        $this->browse(function ($browser) use ($owner) {
+        	$firstOwner = BusinessOwner::first();
+            $browser->loginAs($firstOwner, 'web_admin')
+            	->visit('/admin')
+            	->assertPathIs('/admin')
+                ->assertSee('Summary of Bookings');
         });
     }
 }
