15c1e8d24ac47cf3724405816f0d443bdc3926e5
rmit-s3604367-johnny-huynh <s3604367@student.rmit.edu.au>
2017-03-27 23:52:40 +1100

    Cleaned up database migrations
    Former-commit-id: 879e97410d51ac6fb837e6893d7b82f66f4ca406

--- database/migrations/2017_03_19_081613_create_bookings_table.php
+++ database/migrations/2017_03_19_081613_create_bookings_table.php
@@ -15,7 +15,7 @@ class CreateBookingsTable extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->increments('id');
-			$table->integer('customer_id')->unsigned->index;
+			$table->integer('customer_id')->unsigned;
 			$table->date('booking_start_time');
 			$table->date('booking_end_time');
             $table->timestamps();
