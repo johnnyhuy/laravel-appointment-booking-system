7f26ea7e686ee190031458742e5a2cc42501b18e
Lachlan-Porter <s3537901@student.rmit.edu.au>
2017-03-31 11:00:19 +1100

    added test cases for booking history page
    Former-commit-id: e6dc17d0ddf37810a0fe2a407330f608a39b28c8

---
+++ tests/Integration/BookingHistoryTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Integration;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App\Booking;
+
+class BookingHistory extends TestCase
+{
+	// Rollback database actions once test is complete with this trait
+	use DatabaseTransactions;
+
+    public function testOnlyShowPreviousBookings()
+    {
+        //a previous booking 
+    	$validBooking = factory(Booking::class)->create([
+            'booking_start_date' => Carbon::now()->subWeek()]);
+
+        //a future booking (should not be displayed)
+        $invalidBooking = factory(Booking::class->create([
+            'booking_start_date'] => Carbon::now()->addWeek()));
+
+        $history = Booking::getHistory();
+
+        //check if the valid booking is contained but not the invalid one
+        $this->assertContains($validBooking, $history);
+        $this->assertNotContains($invalidBooking, $history)
+    }
+
+    public function testDontShowNowBookings()
+    {
+        //this means if the booking is exactly now it should not be displayed
+        //a form of boundry testing for the middle since startTime < now
+
+        $nowBooking = factory(Booking::class)->create([
+            'booking_start_date' => Carbon::now()]);
+
+        $history = Booking::getHistory();
+
+        //check if this booking is displayed
+        $this->assertContains($nowBooking, $history);
+    }
+
+    /*
+        Tests we need to fill the arbitrary 6 test quota
+    */
+
+
+    public function testDisplayManyBookings()
+    {
+        //create a few bookings and make sure they are all being displayed
+        $booking = factory(Booking::class, 20)->create([
+            'booking_start_date' => Carbon::now()]);
+
+        $history = Booking::getHistory();
+
+        //make sure they are all returned (since they should all be valid)
+        $this->assertCount(20, $history);
+    }
+
+
+
+
+}
