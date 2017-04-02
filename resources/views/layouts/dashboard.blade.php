62e7a6fabbf3481637bd6ef670451da52f844b3a
rmit-s3601725-craig-barry <s3601725@student.rmit.edu.au>
2017-04-02 16:03:22 +1000

    Add files via upload
    Former-commit-id: 7cd0ee22b9b0dabae2ff868773235eddcaa1fe65

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -35,10 +35,10 @@
 		<div class="row">
 			<div class="col-sm-3 col-md-2 sidebar">
 				<ul class="nav nav-sidebar">
-					<li class="active"><a href="#">Bookings<span class="sr-only">(current)</span></a></li>
+					<li class="active"><a href="#">Summary<span class="sr-only">(current)</span></a></li>
 					<li><a href="#">History</a></li>
-					<li><a href="#">Employees</a></li>
 					<li><a href="#">Roster</a></li>
+					<li><a href="/admin/employees">Employees</a></li>
 				</ul>
 			</div>
 			<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
