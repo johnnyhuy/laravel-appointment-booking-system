7ad6e1d1a55306e31c004749eb8ea18e7d55a311
rmit-s3601725-craig-barry <s3601725@student.rmit.edu.au>
2017-04-02 16:03:51 +1000

    Add files via upload
    Former-commit-id: f4f3c78115933fcf515089868754ecb810fe4ce1

---
+++ resources/views/admin/employees.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.dashboard')
+
+@section('content')
+
+<div class="main__block">
+	<h1 class="main__header">Add Employee</h1>
+	<h4 class="main_description">Add a new employee to the system</h4>
+	@if ($flash = session('message'))
+			<div class="alert alert-success">
+				{{ $flash }}	
+			</div>
+		@endif
+	@if (count($errors))
+		<div class="alert alert-danger">
+			@foreach ($errors->all() as $error)
+				{{ $error }}<br>
+			@endforeach
+		</div>
+	@endif
+	<form method="POST" action="/admin/employees">
+		<!--<div class="block request">-->
+			{{ csrf_field() }}
+			<label for="inputTitle">Title</label>
+			<input name="title" type="text" id="inputTitle" class="form-control request__input" placeholder="Title" value="{{old('title')}}" autofocus>
+			<label for="inputFirstName">First Name</label>
+			<input name="firstname" type="text" id="inputFirstName" class="form-control request__input" placeholder="First Name" value="{{old('firstname')}}" autofocus>
+			<label for="inputLastName">Last Name</label>
+			<input name="lastname" type="text" id="inputLastName" class="form-control request__input" placeholder="Last Name" value="{{old('lastname')}}" autofocus>
+			<label for="inputPhone">Phone <span class="request__validate">(at least 10 characters)</span></label>
+			<input name="phone" type="text" id="inputPhone" class="form-control request__input" placeholder="Phone" value="{{old('phone')}}" autofocus>
+			<br/>
+		<!--</div>-->
+		<button class="btn btn-lg btn-primary btn-block" href="/admin/employees">Register</a>
+	</form>
+</div>
+@endsection
