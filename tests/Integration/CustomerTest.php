a4119607510b43ca5208123d88c0db0862f5adf9
rmit-s3604367-johnny-huynh <s3604367@student.rmit.edu.au>
2017-04-04 13:27:01 +1000

    Added active classes to links and working test cases
    Former-commit-id: 4fe92796caed1d6a5d7e0ff354dcb08f9975726e

--- tests/Integration/CustomerTest.php
+++ tests/Integration/CustomerTest.php
@@ -16,19 +16,13 @@ class CustomerTest extends TestCase
     // Rollback database actions once test is complete with this trait
     use DatabaseTransactions;
 
-    /**
-     * Customer cannot have same username as business owner
-     *
-     * @return void
-     */
-    public function testCustomerCannotHaveSameUsernameAsBusinessOwner()
-    {
-        // Given business owner is created
-        $businessOwner = factory(BusinessOwner::class)->create();
+    protected $customerData;
 
+    protected function setUp() {
         // When customer inputs data
         $customer = factory(Customer::class)->make();
-        $customerData = [
+
+        $this->customerData = [
             'firstname' => $customer->firstname,
             'lastname' => $customer->lastname,
             // Customer has same username as business owner
@@ -39,13 +33,40 @@ class CustomerTest extends TestCase
             'phone' => $customer->phone,
             'address' => $customer->address,
         ];
+    }
+
+    /**
+     * Customer cannot have same username as business owner
+     *
+     * @return void
+     */
+    public function testCustomerCannotHaveSameUsernameAsBusinessOwner()
+    {
+        // Given business owner is created
+        $businessOwner = factory(BusinessOwner::class)->create();
 
         // and send request
-        $response = $this->json('POST', '/register', $customerData);
+        $response = $this->json('POST', '/register', $this->customerData);
 
         // Then respond with an error
         $response->assertJson([
             'username' => ['The username has already been taken.']
         ], 'Customer must not have the same username as business owner username');
+    }
+
+    /**
+     * Tests validation rules for first name field when customer registers
+     *
+     * @return void
+     */
+    public function testCustomerRegisterFirstNameValidation()
+    {
+        // Send post request to /register with included customer input data
+        $response = $this->json('POST', '/register', $customerData);
+
+        $this->customerData->firstname = '';
+
+        // Then respond with an error
+        $response->assertJson(['messages'], 'Customer must not have the same username as business owner username');
     }
 }
