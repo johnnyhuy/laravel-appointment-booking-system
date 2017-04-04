be5f4cc35a769707c2f97e435ebb6ad0d7979f87
rmit-s3600396-jarry-chen <Deaznasdead707s3600396@student.rmit.edu.au>
2017-04-04 23:24:01 +1000

    completed index page
    Former-commit-id: c8867634eddcbbc74b47a7df3d2965c14096e4e0

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,8 +2,15 @@
 
 @section('content')
 	<div class="main__block">
-		<h1 class="main__header">Summary of Bookings</h1>
-		<h4 class="main_description">Present latest bookings.</h4>
+		<h1 class="main__header">Summary of Business</h1>
+		<h2 class="main_description">Details:</h2>
+		<h3 class="main_description">Business name: {{ucfirst($business->business_name) }}</h3>
+		<h3 class="main_description">Business owner: {{ucfirst($business->owner_name) }}</h3>
+		<h2 class="main_description">Contact details:</h2>
+		<h3 class="main_description">
+		Address: {{$business->address }}</h3>
+		<h3 class="main_description">
+		Phone: {{$business->phone }}</h3>
 	
 	</div>
 @endsection
