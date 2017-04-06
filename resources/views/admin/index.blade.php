abe089a651cd26f9e8628486dda0d62d6d2f8689
rmit-s3604367-johnny-huynh <s3604367@student.rmit.edu.au>
2017-04-07 00:19:34 +1000

    Refactored integration test cases, MVC & blade files
    Former-commit-id: 5d390a44e06dcce87132f0cf2c8c5dc494e2d996

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,9 +5,9 @@
 		<h2 class="dash__header">Business Information</h2>
 		<h4 class="main_description">Details of the business</h4>
 		<h2>Full Name </h2>
-		<p class="dash__info">{{ ucfirst($business->owner_name) }}</p>
+		<p class="dash__info">{{ ucfirst($business->firstname) . ' ' . ucfirst($business->lastname) }}</p>
 		<h2>Business name</h2> 
-		<p class="dash__info">{{ ucfirst($business->business_name) }}</p>
+		<p class="dash__info">{{ $business->business_name }}</p>
 		<h2>Phone</h2>
 		<p class="dash__info">{{ $business->phone }}</p>
 		<h2>Address</h2>
