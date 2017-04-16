508a71d8aa6693150f083365692d4731cb29bb9b
Johnny Huynh <s3604367@student.rmit.edu.au>
2017-04-16 18:58:09 +1000

    Hotfix Validator
    Former-commit-id: 39fe303c97227473f2c9cf322e90967de2b27b87

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,27 +24,6 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         parent::boot();
-
-        Validator::extend('is_employee_not_working', function ($attribute, $value, $parameters, $validator) {
-            $date = $parameters[0];
-            $startTime = $parameters[1];
-            $endTime = $parameters[2];
-            $bookings = App\Booking::where('employee_id', $value)
-                ->where('date', '=', $date);
-
-            foreach ($bookings as $booking) {
-                // When a booking is between an employee working
-                if ($booking->start_time < $startTime and $booking->end_time > $endTime) {
-                    return false;
-                }
-
-                if ($booking->start_time < $startTime and $booking->end_time < $endTime) {
-                    return false;
-                }
-            }
-
-            return $count === 0;
-        });
     }
 
     /**
