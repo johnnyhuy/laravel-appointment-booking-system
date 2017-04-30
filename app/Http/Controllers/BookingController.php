3c5d0af4ec56a6ee9ea14361f8e9dda62f45cc8f
rmit-s3601725-craig-barry <s3601725@student.rmit.edu.au>
2017-04-30 11:24:55 +1000

    Add files via upload
    Former-commit-id: a81df599d74f1a819452dc8708cba57d31d55b98

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -15,7 +15,7 @@ class BookingController extends Controller
 {
 	public function __construct() {
 		// Check auth, if not auth then redirect to login
-        $this->middleware('auth:web_user', ['only' => 'customerBookings']);
+        $this->middleware('auth:web_user', ['only' => ['customerBookings', 'showCreateBooking']]);
         $this->middleware('auth:web_admin', [
             'only' => [
                 'index',
@@ -91,12 +91,65 @@ class BookingController extends Controller
         return redirect('/admin/booking');
     }
 
+    public function createBooking(Request $request) {
+        // Validation error messages
+        $messages = [
+            'start_time.date_format' => 'The :attribute field must be in the correct time format.',
+            'end_time.date_format' => 'The :attribute field must be in the correct time format.',
+            'customer_id.exists' => 'The :attribute does not exist.',
+            'activity_id.exists' => 'The :attribute does not exist.',
+            'activity_id.is_end_time_valid' => 'The :attribute duration added on start time is invalid. Please add a start time that does not go to the next day.',
+        ];
+
+        // Validation rules
+        $rules = [
+            'customer_id' => 'required|exists:customers,id',
+            'activity_id' => 'required|exists:activities,id|is_end_time_valid',
+            'start_time' => 'required|date_format:H:i',
+            'date' => 'required|date',
+        ];
+
+        // Attributes replace the field name with a more readable name
+        $attributes = [
+            'customer_id' => 'customer',
+            'activity_id' => 'activity',
+            'start_time' => 'start time',
+            'end_time' => 'end time',
+        ];
+
+        // Validate form
+        $this->validate($request, $rules, $messages, $attributes);
+
+        // Create customer
+        $booking = Booking::create([
+            'customer_id' => $request->customer_id,
+            'activity_id' => $request->activity_id,
+            'start_time' => $request->start_time,
+            'end_time' => Booking::calcEndTime(Activity::find($request->activity_id)->duration, $request->start_time),
+            'date' => $request->date,
+        ]);
+
+        // Session flash
+        session()->flash('message', 'Booking has successfully been created.');
+
+        //Redirect to the business owner admin page
+        return redirect('/create_booking');
+    }
+
 	/**
 	 * View index of customer bookings
 	 */
 	public function customerBookings()
 	{
 		return view('customer.bookings');
+	}
+	
+	/**
+	 * Shows the create booking page
+	 */
+	public function showCreateBooking() 
+	{
+		return view('customer.create_bookings');
 	}
 
     /**
