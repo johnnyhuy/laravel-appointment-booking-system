ff562fb1b88a3b0b227e59eb8c5b422755508611
Johnny <johnnyhuynhdev@gmail.com>
2017-05-05 14:28:40 +1000

    Updated database seed
    Former-commit-id: 62aac83835a13eb6d39a7f0e83bddcc083ca0431

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -57,7 +57,8 @@ class BookingController extends Controller
         // Get bookings of the month
         $bookings = Booking::where('date', '<=', $date->endOfMonth()->toDateString())
             ->where('date', '>=', $date->startOfMonth()->toDateString())
-            ->get();
+            ->get()
+            ->sortBy('date');
 
         // Find employee
         $employee = Employee::findOrFail($employeeID);
